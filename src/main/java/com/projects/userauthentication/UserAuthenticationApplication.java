df98fd5d637bc8e377fbed6d0f5da13a8bc9dae3
mariaarriola <maria.arriola@despegar.com>
2020-03-22 12:39:13 -0300

    First commit - project creation

---
+++ src/main/java/com/projects/userauthentication/UserAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.projects.userauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationApplication.class, args);
+	}
+
+}
